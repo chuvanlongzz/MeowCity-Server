15bb59b7b687c8bf5873b5bdb9a391ff5f7266f0
chuvanlongzz <chu96978@gmail.com>
2026-04-14 01:02:59 +0700

    Up code MeowCity

---
+++ resources/loading_screen/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'music.mp3',
+    'anh1.jpg',
+    'anh2.jpg',
+    'anh3.jpg',
+    'anh4.jpg',
+    'anh5.jpg',
+    'anh6.jpg',
+    'anh7.jpg',
+}
